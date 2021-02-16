8c42e29c5c325cf0e01db3e065db3294d3f507f9
CCheukKa <ccheukka@gmail.com>
2021-02-17 07:33:16 +0800

    Added option to copy email address to clipboard + Some CSS fix

--- home/home.js
+++ home/home.js
@@ -1,4 +1,19 @@
-return
-setTimeout(() => {
-    location.reload();
-}, 1000);
+const emailAddressContainer = document.getElementById('emailAddressContainer');
+
+emailAddressContainer.addEventListener('click', function(e) {
+    const el = document.createElement('textarea');
+    el.value = 'contact.CCheukKa@gmail.com';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    console.log('Copied')
+    document.body.removeChild(el);
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "✔️ Copied! ";
+});
+
+emailAddressContainer.addEventListener('mouseleave', function(e) {
+    console.log('Mouse leave');
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "📋 Copy to clipboard";
+});
