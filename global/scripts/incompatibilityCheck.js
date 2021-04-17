13f88213eb43c53d86e2dfc4849a69c41c8d3be8
CCheukKa <ccheukka@gmail.com>
2021-04-17 09:14:02 +0800

    Added link to patreon in ./home;\nFixed mobile incompatibility modal;\nAdded browser incompatibility severity scale

--- global/scripts/incompatibilityCheck.js
+++ global/scripts/incompatibilityCheck.js
@@ -1,5 +1,11 @@
 //! <script src="https://cdn.jsdelivr.net/npm/bowser@2.9.0/es5.js"></script>
 
+const compatibleBrowserList = [
+    'Chrome',
+    'Microsoft Edge',
+    'Opera'
+];
+
 window.addEventListener("load", function() {
     // (B1) PARSE USER AGENT
     var result = bowser.getParser(navigator.userAgent).getResult();
@@ -18,6 +24,7 @@ window.addEventListener("load", function() {
     //console.log(result.platform.type);
 
     //
+    result.severity = severity(result.browser.name);
     console.log(result);
     //
 
@@ -33,7 +40,7 @@ function showWarning(isMobile, message) {
     setTimeout(() => {
         document.getElementById(`modalbox-title`).innerHTML = message.title;
         document.getElementById(`modalbox-body`).innerHTML = message.body.replaceAll('\n', '<br>');
-        document.getElementById(`modalbox-button-text`).innerHTML = message.buttonText;
+        document.getElementById(`modalbox-button-text`).innerHTML = message.buttonText.replaceAll('\n', '<br>');
         document.getElementById(`modalButton`).click();
     }, 10);
     return;
@@ -44,15 +51,22 @@ function check(result) {
     if (isMobile) {
         showWarning(isMobile, {
             title: 'Incompatibility Warning!',
-            body: 'Mobile browser detected!\nYou are in potentially-incompatible territories. If you wish to continue, some things may break!\nYou have been warned!',
-            buttonText: 'ÒωÓ  Am stubborn; me wants in!'
+            body: 'Mobile browser detected!\nYou are in potentially-incompatible territories.\nIf you wish to continue, some things may break!\nYou have been warned!',
+            buttonText: 'ÒωÓ  Am stubborn;\nme wants in!'
         });
     }
-    if (!insensitiveIsEqual(result.browser.name, "Chrome")) {
+    if (!compatibleBrowserList.includes(result.browser.name)) {
         showWarning(isMobile, {
             title: 'Incompatibility Warning!',
-            body: `${result.browser.name} browser detected!\nThis website is developed and tested mainly on Google Chrome. I am trying my best to provide compatibility, but there are no promises! Some things may not display properly.\nYou have been warned.`,
+            body: `${result.browser.name} browser detected!\nThis website is developed and tested mainly on Google Chrome. I am trying my best to provide compatibility, but there are no promises! Some things may not display properly.\nYou have been warned.\nIncompatibility severity: ${result.severity}`,
             buttonText: "I'll take my chances"
         });
     }
+}
+
+function severity(browserName) {
+    if (insensitiveIsEqual(browserName, "Firefox")) {
+        return '★★★★☆ 4/5';
+    }
+    return '(Untested/Unknown browser)';
 }
