d41df1f7ab549504cbc53f3dca93bacad10ecc5f
CCheukKa <ccheukka@gmail.com>
2021-08-03 10:31:20 +0800

    Semi-fixed dropdown nav menu not being responsive

--- global/scripts/globalResizeHandler.js
+++ global/scripts/globalResizeHandler.js
@@ -1,20 +1,17 @@
-{
-    const headerElement = document.getElementById('header');
-    const titleElement = document.getElementById('title');
-    const catalogueElement = document.getElementById('catalogue');
-    const dropdownElement = document.getElementById('dropdown');
+const headerElement = document.getElementById('header');
+const titleElement = document.getElementById('title');
+const dropdownElement = document.getElementById('dropdown');
 
-    window.addEventListener('load', resizeHandler);
-    window.addEventListener('resize', resizeHandler);
+window.addEventListener('load', resizeHandler);
+window.addEventListener('resize', resizeHandler);
 
 
-    function resizeHandler() {
-        //! Header
-        catalogueElement.hidden = false;
-        dropdownElement.hidden = true;
-        let useDropdownMenu = (titleElement.getBoundingClientRect().right + 40 >= catalogueElement.getBoundingClientRect().left);
-        catalogueElement.hidden = useDropdownMenu;
-        dropdownElement.hidden = !useDropdownMenu;
-    }
-
+function resizeHandler() {
+    //! Header
+    catalogueElement.hidden = false;
+    dropdownElement.hidden = true;
+    let useDropdownMenu = (titleElement.getBoundingClientRect().right + 40 >= catalogueElement.getBoundingClientRect().left);
+    console.log(titleElement.getBoundingClientRect().right, catalogueElement.getBoundingClientRect().left);
+    catalogueElement.hidden = useDropdownMenu;
+    dropdownElement.hidden = !useDropdownMenu;
 }
