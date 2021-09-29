e0dc2eb9ac7351f25771cc3d399fe62996333ec0
CCheukKa <ccheukka@gmail.com>
2021-09-30 01:21:59 +0800

    Added async http get function

--- global/scripts/globalLibrary.js
+++ global/scripts/globalLibrary.js
@@ -72,3 +72,13 @@ function onLoadPage() {
     window.universal.style.transition = "all 0.3s";
     return;
 }
+
+function httpGetAsync(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
