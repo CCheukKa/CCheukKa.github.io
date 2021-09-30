8bbb89a9e5084e58095e7acb41efb43bc9aaf9b7
CCheukKa <ccheukka@gmail.com>
2021-09-30 16:56:20 +0800

    Added option to enable light mode in /journal

--- journal/journal.js
+++ journal/journal.js
@@ -1,4 +1,7 @@
 const text = document.getElementById('text-content');
+const lightModeStylesheet = document.getElementById('light-mode-stylesheet');
+var useDarkMode = true;
+lightModeStylesheet.disabled = useDarkMode;
 
 httpGetAsync("https://raw.githubusercontent.com/CCheukKa/CCheukKa/master/Journal.md", response => {
     // console.log(response);
@@ -8,6 +11,7 @@ httpGetAsync("https://raw.githubusercontent.com/CCheukKa/CCheukKa/master/Journal
 
 function parseResponse(response) {
 
+    response = response.replaceAll('<!-- omit in toc -->', '');
     var reader = new commonmark.Parser();
     var writer = new commonmark.HtmlRenderer();
     var parsed = reader.parse(response);
@@ -28,4 +32,10 @@ function addSectionTags(container) {
         element.setAttribute('id', element.innerHTML.toLowerCase().replaceAll(' ', '-'));
         // console.log(element);
     });
+}
+
+function toggleDarkMode(icon) {
+    useDarkMode = !useDarkMode;
+    icon.innerHTML = useDarkMode ? '🔆' : '🔅';
+    lightModeStylesheet.disabled = useDarkMode;
 }
