6ce6265167727c5adb99a2030dc337610d464dae
CCheukKa <ccheukka@gmail.com>
2021-10-10 00:50:44 +0800

    Fixed minor bug

--- journal/journal.js
+++ journal/journal.js
@@ -2,16 +2,27 @@ const text = document.getElementById('text-content');
 const lightModeStylesheet = document.getElementById('light-mode-stylesheet');
 var useDarkMode = true;
 lightModeStylesheet.disabled = useDarkMode;
+const fonts = [
+    `Georgia, 'Times New Roman', Times, serif`,
+    `'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif`,
+    `'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif`,
+    `'Segoe UI', Tahoma, Geneva, Verdana, sans-serif`,
+    `Verdana, Geneva, Tahoma, sans-serif`,
+];
+var font = 0;
 
 httpGetAsync("https://raw.githubusercontent.com/CCheukKa/CCheukKa/master/Journal.md", response => {
     // console.log(response);
     text.innerHTML = parseResponse(response);
     addSectionTags(text);
+    buildNavigation(text);
     //
     console.log(`Done journal assembly`);
-    let redirect = `#${window.location.href.split('#').slice(-1)}`;
-    window.location = redirect;
-    console.log(`Redirected to ${redirect}`);
+    if (window.location.href.split('#')[1]) {
+        let redirect = `#${window.location.href.split('#').slice(-1)}`;
+        window.location = redirect;
+        console.log(`Redirected to ${redirect}`);
+    }
     //
 });
 
@@ -46,15 +57,6 @@ function toggleDarkMode(icon) {
     lightModeStylesheet.disabled = useDarkMode;
 }
 
-const fonts = [
-    `Georgia, 'Times New Roman', Times, serif`,
-    `'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif`,
-    `'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif`,
-    `'Segoe UI', Tahoma, Geneva, Verdana, sans-serif`,
-    `Verdana, Geneva, Tahoma, sans-serif`,
-];
-var font = 0;
-
 function cycleFonts(button) {
     font = (font + 1) % fonts.length;
     button.style.fontFamily = fonts[font];
@@ -63,4 +65,8 @@ function cycleFonts(button) {
     });
 
     console.log(fonts[font]);
+}
+
+function buildNavigation(text) {
+
 }
