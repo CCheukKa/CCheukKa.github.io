8e0801363082bea3dec57e10b4c3818d1f751971
CCheukKa <ccheukka@gmail.com>
2022-11-24 05:38:26 +0800

    Implemented clickable months for calendar

--- journal/journal.js
+++ journal/journal.js
@@ -252,6 +252,7 @@ function CalendarControl() {
         displayYearMonth: function () {
             let yearMonthLabel = document.querySelector(".calendar .calendar-year-month-label");
             yearMonthLabel.innerHTML = `${calendarControl.calMonthName[calendar.getMonth()]} - ${calendar.getFullYear()}`;
+            yearMonthLabel.href = `#${calendarControl.calMonthName[calendar.getMonth()]}---${calendar.getFullYear()}`.toLowerCase();
         },
         selectDate: function (e) {
             console.log(`${(+e.target.textContent + 100).toString().substring(1)}-${calendar.getMonth() + 1}-${calendar.getFullYear()}`);
@@ -262,7 +263,7 @@ function CalendarControl() {
             ).innerHTML += `<div class="calendar-inner"><div class="calendar-controls">
                             <div class="calendar-prev"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 128 128"><path fill="#666" d="M88.2 3.8L35.8 56.23 28 64l7.8 7.78 52.4 52.4 9.78-7.76L45.58 64l52.4-52.4z"/></svg></a></div>
                             <div class="calendar-year-month">
-                            <div class="calendar-year-month-label"></div>
+                            <a class="calendar-year-month-label"></a>
                             </div>
                             <div class="calendar-next"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 128 128"><path fill="#666" d="M38.8 124.2l52.4-52.42L99 64l-7.77-7.78-52.4-52.4-9.8 7.77L81.44 64 29 116.42z"/></svg></a></div>
                             </div>
