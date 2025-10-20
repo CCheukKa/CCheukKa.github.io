3b80f0e3838c9a9f11049945f6ff847aba0b80cf
CCheukKa <ccheukka@gmail.com>
2025-10-20 23:55:31 +0800

    initialise Next.js app

--- projects/chess/vectorised-western-chess/scripts/listener.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('load', redrawChess(), getBoardColours());
-s.addEventListener('mousedown', e => startDrag(e));
-s.addEventListener('mousemove', e => drag(e));
-s.addEventListener('mouseup', e => endDrag(e));
--- projects/connect-four/scripts/listener.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('load', redrawBoard(), getBoardColours());
-s.addEventListener('mousemove', e => mouseMove(e));
-s.addEventListener('click', e => click(e));
-s.addEventListener('mouseleave', e => mouseLeave(e));
