a0ba23baba2501ba4a6b7a3b08b7ac059a02f995
CCheukKa <ccheukka@gmail.com>
2025-10-21 23:21:56 +0800

    change output to SSG

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+    /* config options here */
+    reactStrictMode: true,
+    output: 'export',
 };
 
 export default nextConfig;
