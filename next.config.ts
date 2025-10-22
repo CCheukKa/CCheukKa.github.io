f98862d7fd0c9d52c55890861d434db3e306af93
CCheukKa <ccheukka@gmail.com>
2025-10-23 00:28:13 +0800

    fix: update distribution directory in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
     /* config options here */
     reactStrictMode: true,
     output: 'export',
-    distDir: 'build',
+    distDir: 'out',
     productionBrowserSourceMaps: true,
 };
 
