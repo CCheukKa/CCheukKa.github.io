fb80792cf6a47bfb0794587eda209c20341880e3
CCheukKa <ccheukka@gmail.com>
2025-10-22 19:37:40 +0800

    feat: add plasticityConfig and Plasticity page with Shelf and TitleCard components

---
+++ src/pages/plasticity.tsx
@@ -0,0 +1,23 @@
+import Shelf from "@/components/shelf";
+import TitleCard from "@/components/titleCard";
+import { plasticityConfig } from "@/configs/plasticityConfig";
+
+export default function Plasticity() {
+    return (
+        <>
+            <title>Plasticity</title>
+
+            <TitleCard
+                title="Plasticity"
+                flavourText="Random dumb pieces of texts I sometimes write"
+                description={
+                    "Sometimes I just couldn't help myself when some obscure inspiration strikes; I would find myself writing non-stop. The topics they explore are as random and disorganised as can be."
+                    + "\n" +
+                    "Simply put, these are all just low-quality brain vomits and aimless rambles with little regard for grammar."
+                }
+            />
+
+            <Shelf shelfConfig={plasticityConfig} />
+        </>
+    );
+}
