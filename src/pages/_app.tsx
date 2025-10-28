a02a072e9e48f655b88e7a639225161d87b4d770
CCheukKa <ccheukka@gmail.com>
2025-10-29 01:56:47 +0800

    feat: update font families across styles for improved consistency and typography

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,16 @@ import Header from "@/components/Header";
 import "@/styles/globals.css";
 import { useRef } from "react";
 import LayoutContext from "@/context/LayoutContext";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
     const mainRef = useRef<HTMLElement>(null);
     return (
         <LayoutContext.Provider value={{ mainRef }}>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+            </Head>
             <Header />
             <main ref={mainRef}>
                 <Component {...pageProps} />
