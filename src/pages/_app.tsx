da5624aafaa584914b78d54d9855b3e7253df33f
CCheukKa <ccheukka@gmail.com>
2025-10-29 00:54:56 +0800

    feat: add LayoutContext for managing mainRef in application layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import type { AppProps } from "next/app";
 import Header from "@/components/Header";
 import "@/styles/globals.css";
+import { useRef } from "react";
+import LayoutContext from "@/context/LayoutContext";
 
 export default function App({ Component, pageProps }: AppProps) {
+    const mainRef = useRef<HTMLElement>(null);
     return (
-        <>
+        <LayoutContext.Provider value={{ mainRef }}>
             <Header />
-            <main>
+            <main ref={mainRef}>
                 <Component {...pageProps} />
             </main>
-        </>
+        </LayoutContext.Provider>
     );
 }
