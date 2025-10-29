657371f6fc9b84689853cd8af24c444a2fe84218
CCheukKa <ccheukka@gmail.com>
2025-10-29 19:30:20 +0800

    feat: add flags

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    reactStrictMode: true,
-    output: 'export',
     distDir: 'out',
+    generateBuildId: async () => {
+        return process.env.GIT_HASH
+            ? `GIT_HASH_${process.env.GIT_HASH}`
+            : `BUILD_TIME_${Date.now().toString(36)}`;
+    },
+    output: 'export',
     productionBrowserSourceMaps: true,
     reactCompiler: true,
+    reactStrictMode: true,
+    typedRoutes: true,
 };
 
 export default nextConfig;
