8f6bc0389345402778eed9a5d2393970e1ab125a
CCheukKa <ccheukka@gmail.com>
2025-11-11 02:47:15 +0800

    feat: update portfolioConfig with new CUHK BME promotion materials and adjust existing entries

--- src/configs/portfolioConfig.ts
+++ src/configs/portfolioConfig.ts
@@ -3,6 +3,7 @@ import type { ShelfConfig, ShelfItem } from "./shelfConfig"
 export enum PortfolioProjectCategory {
     STEM_WORKSHOP_MATERIALS = "STEM Workshop Materials",
     RESEARCH_PROJECTS = "Research Projects",
+    CUHK_BME_PROMOTION_MATERIALS = "CUHK BME Promotion Materials",
     STUDENT_SOCIETY_MATERIALS = "Student Society Materials",
 }
 
@@ -26,17 +27,9 @@ export const portfolioConfig: Omit<ShelfConfig<PortfolioShelfItem>, "shelfCatego
         {
             isRemote: true,
             openInNewTab: true,
-            displayName: "Vacuum Workshop Slides",
-            refPath: "https://www.canva.com/design/DAGk0UIFDqc/jOlMpj4_xQtFZitazNjueA/view",
-            thumbnailPathOverride: "https://cck.wtf/portfolioAssets/vacuum-workshop/title.png",
-            projectCategory: PortfolioProjectCategory.STEM_WORKSHOP_MATERIALS,
-            mediumCategory: PortfolioMediumCategory.PRESENTATION_SLIDES,
-        }, {
-            isRemote: true,
-            openInNewTab: true,
-            displayName: "Vacuum Workshop Guide",
-            refPath: "https://cck.wtf/portfolioAssets/vacuum-workshop/guide.pdf",
-            thumbnailPathOverride: "https://cck.wtf/portfolioAssets/vacuum-workshop/guide.png",
+            displayName: "Catapult Workshop Handout",
+            refPath: "https://cck.wtf/portfolioAssets/catapult-workshop/handout.pdf",
+            thumbnailPathOverride: "https://cck.wtf/portfolioAssets/catapult-workshop/handout.png",
             projectCategory: PortfolioProjectCategory.STEM_WORKSHOP_MATERIALS,
             mediumCategory: PortfolioMediumCategory.GRAPHIC_DESIGN_AND_ANIMATION,
         }, {
@@ -65,11 +58,27 @@ export const portfolioConfig: Omit<ShelfConfig<PortfolioShelfItem>, "shelfCatego
         }, {
             isRemote: true,
             openInNewTab: true,
-            displayName: "Catapult Workshop Handout",
-            refPath: "https://cck.wtf/portfolioAssets/catapult-workshop/handout.pdf",
-            thumbnailPathOverride: "https://cck.wtf/portfolioAssets/catapult-workshop/handout.png",
+            displayName: "Vacuum Workshop Slides",
+            refPath: "https://www.canva.com/design/DAGk0UIFDqc/jOlMpj4_xQtFZitazNjueA/view",
+            thumbnailPathOverride: "https://cck.wtf/portfolioAssets/vacuum-workshop/title.png",
+            projectCategory: PortfolioProjectCategory.STEM_WORKSHOP_MATERIALS,
+            mediumCategory: PortfolioMediumCategory.PRESENTATION_SLIDES,
+        }, {
+            isRemote: true,
+            openInNewTab: true,
+            displayName: "Vacuum Workshop Guide",
+            refPath: "https://cck.wtf/portfolioAssets/vacuum-workshop/guide.pdf",
+            thumbnailPathOverride: "https://cck.wtf/portfolioAssets/vacuum-workshop/guide.png",
             projectCategory: PortfolioProjectCategory.STEM_WORKSHOP_MATERIALS,
             mediumCategory: PortfolioMediumCategory.GRAPHIC_DESIGN_AND_ANIMATION,
+        }, {
+            isRemote: true,
+            openInNewTab: true,
+            displayName: "InnoCarnival Q&A Game",
+            refPath: "https://cck.wtf/Green-STEM-Q-and-A-game",
+            refPage: "page.html",
+            thumbnailPathOverride: "https://cck.wtf/portfolioAssets/green-stem-innocarnival-game/game.png",
+            mediumCategory: PortfolioMediumCategory.WEBSITE,
         },
 
         //^ ResiSense
@@ -100,11 +109,45 @@ export const portfolioConfig: Omit<ShelfConfig<PortfolioShelfItem>, "shelfCatego
         }, {
             isRemote: true,
             openInNewTab: true,
-            displayName: "InnoCarnival Q&A Game",
-            refPath: "https://cck.wtf/Green-STEM-Q-and-A-game",
-            refPage: "page.html",
-            thumbnailPathOverride: "https://cck.wtf/portfolioAssets/green-stem-innocarnival-game/game.png",
-            mediumCategory: PortfolioMediumCategory.WEBSITE,
+            displayName: "ResiSense Presentation Slides",
+            refPath: "https://www.figma.com/proto/CXkUXf3W9ccRuE5iuGOnCD/Symposium-Material?node-id=100-3",
+            thumbnailPathOverride: "https://cck.wtf/portfolioAssets/resisense/symposium.png",
+            projectCategory: PortfolioProjectCategory.RESEARCH_PROJECTS,
+            mediumCategory: PortfolioMediumCategory.PRESENTATION_SLIDES,
+        },
+
+        //^ CUHK BME
+        {
+            isRemote: true,
+            openInNewTab: true,
+            displayName: "BME Programme Video",
+            refPath: "https://www.youtube.com/watch?v=ybDdRa6nVQc",
+            thumbnailPathOverride: "https://img.youtube.com/vi/ybDdRa6nVQc/sddefault.jpg",
+            projectCategory: PortfolioProjectCategory.CUHK_BME_PROMOTION_MATERIALS,
+            mediumCategory: PortfolioMediumCategory.VIDEO_PRODUCTION,
+        }, {
+            isRemote: true,
+            openInNewTab: true,
+            displayName: "BME Interview Video",
+            refPath: "https://www.instagram.com/p/DMH99Zotj6w",
+            thumbnailPathOverride: "https://cck.wtf/portfolioAssets/bme-dept/ricky-interview.pdf",
+            projectCategory: PortfolioProjectCategory.CUHK_BME_PROMOTION_MATERIALS,
+            mediumCategory: PortfolioMediumCategory.VIDEO_PRODUCTION,
+        }, {
+            isRemote: true,
+            openInNewTab: true,
+            displayName: "BME Summer Training Video",
+            refPath: "https://www.instagram.com/p/DMMaiDLSpsL",
+            thumbnailPathOverride: "https://cck.wtf/portfolioAssets/bme-dept/summer-training.pdf",
+            projectCategory: PortfolioProjectCategory.CUHK_BME_PROMOTION_MATERIALS,
+            mediumCategory: PortfolioMediumCategory.VIDEO_PRODUCTION,
+        }, {
+            isRemote: true,
+            openInNewTab: true,
+            displayName: "Department Booklet",
+            refPath: "https://cck.wtf/portfolioAssets/bme-dept/booklet.pdf",
+            projectCategory: PortfolioProjectCategory.CUHK_BME_PROMOTION_MATERIALS,
+            mediumCategory: PortfolioMediumCategory.GRAPHIC_DESIGN_AND_ANIMATION,
         },
 
         //^ Flaretrikos
@@ -113,13 +156,6 @@ export const portfolioConfig: Omit<ShelfConfig<PortfolioShelfItem>, "shelfCatego
             openInNewTab: true,
             displayName: "Society Handbill",
             refPath: "https://cck.wtf/portfolioAssets/flaretrikos/handbill.pdf",
-            projectCategory: PortfolioProjectCategory.STUDENT_SOCIETY_MATERIALS,
-            mediumCategory: PortfolioMediumCategory.GRAPHIC_DESIGN_AND_ANIMATION,
-        }, {
-            isRemote: true,
-            openInNewTab: true,
-            displayName: "Department Booklet",
-            refPath: "https://cck.wtf/portfolioAssets/flaretrikos/booklet.pdf",
             projectCategory: PortfolioProjectCategory.STUDENT_SOCIETY_MATERIALS,
             mediumCategory: PortfolioMediumCategory.GRAPHIC_DESIGN_AND_ANIMATION,
         }, {
@@ -149,6 +185,6 @@ export const portfolioConfig: Omit<ShelfConfig<PortfolioShelfItem>, "shelfCatego
             displayName: "Society Posts (Some)",
             refPath: "https://www.instagram.com/cuhk_flaretrikos",
             projectCategory: PortfolioProjectCategory.STUDENT_SOCIETY_MATERIALS,
-        }
+        },
     ]
 }
